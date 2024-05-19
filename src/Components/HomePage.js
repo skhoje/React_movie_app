a8fe7abcc76690f7ca8430e0fa879be6fe98b6ed
skhoje <96111350+skhoje@users.noreply.github.com>
2024-05-19 23:17:40 +0530

    Update HomePage.js

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,27 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import ApiService from '../Services/ApiService';
 import { Link } from 'react-router-dom';
-import Pagination from './Pagination'; 
+
 
 const HomePage = () => {
   const [popularMovies, setPopularMovies] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
-
+ 
   useEffect(() => {
     const fetchPopularMovies = async (page) => {
-      const { movies, total_pages } = await ApiService.fetchPopularMovies(page);
+
       setPopularMovies(movies);
-      setTotalPages(total_pages);
-    };
+     }, []);
 
-    fetchPopularMovies(currentPage);
-  }, [currentPage]);
-
-  const handlePageChange = (page) => {
-    setCurrentPage(page);
-  };
-
+  
   return (
     <div className="container">
       <h2>Popular Movies</h2>
@@ -37,7 +28,7 @@ const HomePage = () => {
           </Link>
         ))}
       </div>
-      <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
+      
     </div>
   );
 };
