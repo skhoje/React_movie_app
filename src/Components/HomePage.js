30d5697ea23d4bb79c21100d2efab2c6a6ccdc8a
skhoje <96111350+skhoje@users.noreply.github.com>
2024-05-19 23:06:13 +0530

    Update HomePage.js

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,18 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import ApiService from '../Services/ApiService';
 import { Link } from 'react-router-dom';
+import Pagination from './Pagination'; 
 
 const HomePage = () => {
   const [popularMovies, setPopularMovies] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
   useEffect(() => {
-    const fetchPopularMovies = async () => {
-      const movies = await ApiService.fetchPopularMovies();
+    const fetchPopularMovies = async (page) => {
+      const { movies, total_pages } = await ApiService.fetchPopularMovies(page);
       setPopularMovies(movies);
+      setTotalPages(total_pages);
     };
 
-    fetchPopularMovies();
-  }, []);
+    fetchPopularMovies(currentPage);
+  }, [currentPage]);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
 
   return (
     <div className="container">
@@ -20,15 +28,16 @@ const HomePage = () => {
       <div className="movie-list">
         {popularMovies.map(movie => (
           <Link key={movie.id} to={`/movie/${movie.id}`}>
-          <div key={movie.id} className="movie-card">
-            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-            <div className="movie-name">
-              <h3>{movie.title}</h3>
+            <div key={movie.id} className="movie-card">
+              <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+              <div className="movie-name">
+                <h3>{movie.title}</h3>
+              </div>
             </div>
-          </div>
           </Link>
         ))}
       </div>
+      <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
     </div>
   );
 };
